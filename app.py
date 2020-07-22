cde4272e856fc8cc9ccfb1ac9a9dd67bdd7eab52
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-22 23:52:35 +0530

    Added primary action selectbox

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ def run_app():
         '<h1>Deep Learning Studio</h1><hr><br>',
         unsafe_allow_html=True
     )
-    inference_branch_app()
+    action_option = st.selectbox(
+        'What\'d yeh wanna do Harry?',
+        ('', 'Inference on Pre-trained Model', 'Transfer Learning', 'Inference (Not Implemented)')
+    )
+    if action_option == 'Inference on Pre-trained Model':
+        inference_branch_app()
 
 
 if __name__ == '__main__':
